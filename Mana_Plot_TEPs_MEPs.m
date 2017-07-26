ba864cd2a9e314d72c3582f67aa122a80b955b0a
manabm <mana.biabani@gmail.com>
2017-07-26 12:27:29 +1000

    Seperate Y axes for TEPs and MEPs

--- Mana_Plot_TEPs_MEPs.m
+++ Mana_Plot_TEPs_MEPs.m
@@ -1,6 +1,8 @@
-% needs work
+clear; close all; clc; 
+%### Plot MEPs vs EEG continuous recording
 cd '/Volumes/BACKUP_HD/MANA_TMS_EEG/Analyzed'
-load('meps_results.mat')
+load('spTEPs.mat');
+load('ppTMS_MEPs.mat');
 EEG1 = pop_loadset('filename','003_FINAL_high_avref.set','filepath','/Volumes/BACKUP_HD/MANA_TMS_EEG/Analyzed/003/');
 EEG2 = pop_loadset('filename','003_FINAL_low_avref.set','filepath','/Volumes/BACKUP_HD/MANA_TMS_EEG/Analyzed/003/');
 
@@ -15,7 +17,7 @@ error_low = sqrt(mean(MEPs_high(:,:,:)));
 %High intensity TEPs MEPs for one subject(003)
 figure; 
 x = plot(EEG1.times,mean(EEG1.data(5,:,:),3),'b'); hold on;
-%yyaxis left      
+yyaxis left      
 set(gca,'xlim',[-100,300]);
 y = errorbar(timepoints,mean(MEPs_high(:,:,:)),error_high,'s');hold on;
 xlabel('Time(ms)');
@@ -24,9 +26,8 @@ ylabel('Amp(uV)')
 %low intensity TEPs MEPs for one subject(003)
 figure; 
 x = plot(EEG2.times,mean(EEG2.data(5,:,:),3),'b'); hold on;
-%yyaxis left      
+yyaxis left      
 set(gca,'xlim',[-100,300]);
 y = errorbar(timepoints,mean(MEPs_low(:,:,:)),error_low,'s');hold on;
 xlabel('Time(ms)');
 ylabel('Amp(uV)')
-            
