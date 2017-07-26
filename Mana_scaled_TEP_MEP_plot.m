5bd631a8173553324569171f1d8cc948837e7a5b
manabm <mana.biabani@gmail.com>
2017-07-26 12:22:07 +1000

    save directory changed

--- Mana_scaled_TEP_MEP_plot.m
+++ Mana_scaled_TEP_MEP_plot.m
@@ -24,30 +24,29 @@ end
 
 % plot scaled TEPs vs MEPs for allsubjects for each channel and each
 % condition and save the plots 
-cd '/Volumes/BACKUP_HD/MANA_TMS_EEG/Analyzed/fitting/plots'
 
 for j = 1:numofchannels
     
-    figure;
+    h = figure;
     plot(scaled_EEG_timepoints_high_allsubjects(j,:),'r');hold on,
     plot(MEP_vector_high_allsubjects,'b');hold on,
     title (['scaled high channel', (num2str(j))]);hold on,
     set(gca,'Xtick', [1 2 3 4 5 6 7 8], 'XTickLabel',[15 30 45 55 100 120 180 220]); hold on,
-    xlabel('Time');hold on,
-    ylabel('Amplitude')
+    xlabel('Time');
+    ylabel('Amplitude');
     legend ('TEP', 'MEP');
-    savefig(figure,['scaled_high_channel_' (num2str(j))]);
+    saveas (h,fullfile('/Volumes/BACKUP_HD/MANA_TMS_EEG/Analyzed/fitting/plots',['scaled_high_channel_' (num2str(j))]));
     
-    figure;
+    l = figure;
     plot(scaled_EEG_timepoints_low_allsubjects(j,:),'r');hold on,
     plot(MEP_vector_low_allsubjects,'b');hold on,
     title (['scaled low channel', (num2str(j))]);hold on,
     set(gca,'Xtick', [1 2 3 4 5 6 7 8], 'XTickLabel',[15 30 45 55 100 120 180 220]); hold on,
     xlabel('Time');hold on,
-    ylabel('Amplitude')
+    ylabel('Amplitude');
     legend ('TEP', 'MEP');
-    savefig(figure,['scaled_low_channel_' (num2str(j))]);
+    saveas (l,fullfile('/Volumes/BACKUP_HD/MANA_TMS_EEG/Analyzed/fitting/plots',['scaled_low_channel_' (num2str(j))])); 
     
 end
     
-    
+ 
