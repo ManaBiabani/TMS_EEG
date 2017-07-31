7c2f4c72d14e2f591bc66eccfe014a9b63a6dc0f
manabm <mana.biabani@gmail.com>
2017-08-01 09:07:49 +1000

    Topography of p values of correlations between TEPs and MEPs in different ISIs

---
+++ topoplot_corr_pval.m
@@ -0,0 +1,24 @@
+clear; close all; clc;
+
+%## 
+
+% Make the scatter plot of all individuals TEP/MEP values for each channel/ISI/conditioin ( all ISIs in one figure) and save the plots
+ load ('/Volumes/BACKUP_HD/MANA_TMS_EEG/Analyzed/Correlations_MEPs_TEPs_allchannels');
+
+
+for         int = 1:length(cond)
+    
+
+            for  isi = 1:length(ISIs)
+                 topo =  figure;
+                 topoplot(Pval(:,isi,int),EEG_high.chanlocs);
+                 if int == 1
+                 title([ num2str(ISIs(isi)),'(high)']);
+                 else
+                 title([num2str(ISIs(isi)),'(Low)']);
+                 end
+                 saveas(topo,fullfile('/Volumes/BACKUP_HD/MANA_TMS_EEG/Analyzed/myplots/topoplots/',['topo_pval_' cond{int}, '_' num2str(isi) ]));
+           
+            end
+          
+ end
