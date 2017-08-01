c1266d2a6db287cf93433d86cba8e11d2fcb90a3
manabm <mana.biabani@gmail.com>
2017-08-01 11:31:21 +1000

    Topoplot  of r values in correlation tests between MEPs and TEPs

---
+++ Mana_topoplot_corr_r.m
@@ -0,0 +1,25 @@
+clear; close all; clc;
+
+%## 
+
+% Make the scatter plot of all individuals TEP/MEP values for each channel/ISI/conditioin ( all ISIs in one figure) and save the plots
+ load ('/Volumes/BACKUP_HD/MANA_TMS_EEG/Analyzed/Correlations_MEPs_TEPs_allchannels');
+
+
+for         int = 1:length(cond)
+    
+
+            for  isi = 1:length(ISIs)
+                 topo =  figure;
+                 A = topoplot(correlation_results_allchannels(:,isi,int),EEG_high.chanlocs,'colormap', colormap(correlation_results_allchannels(:,isi,int))) );hold on
+                 
+                 if int == 1
+                 title([ num2str(ISIs(isi)),'(high)']);
+                 else
+                 title([num2str(ISIs(isi)),'(Low)']);
+                 end
+                 saveas(topo,fullfile('/Volumes/BACKUP_HD/MANA_TMS_EEG/Analyzed/myplots/topoplots/',['topo_r_' cond{int}, '_' num2str(isi) ]));
+           
+            end
+          
+ end
