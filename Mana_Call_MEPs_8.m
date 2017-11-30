ea80d5febd7d65d3f2ec1c569829e937ebdf6953
manabm <mana.biabani@gmail.com>
2017-11-30 17:46:26 +1100

    some comments added

--- Mana_Call_MEPs_8.m
+++ Mana_Call_MEPs_8.m
@@ -1,15 +1,21 @@
 clear; close all; clc;
 
-% ###### STEP 8 : Call analyzed MEPs from the excel file #####
+% ###### STEP 8 : Call analyzed MEPs from the specified excel file and range#####
 
 pathOut = '/Volumes/BACKUP_HD/MANA_TMS_EEG/Analyzed/';
 
-% ISIs
+% Define ISIs
 ISIs = [15,30,45,55,100,120,180,220];
 
-% Read analyzed MEPs data from the specified worksheet and range
+% Define path where the file is stored
 addpath('/Volumes/BACKUP_HD/MANA_TMS_EEG');
+
+% Specify worksheet and range
 MEPs_low = xlsread('MEP_analysis','MAT', 'B2:I21');
 MEPs_high = xlsread('MEP_analysis','MAT', 'L2:S21');
+
+%store all MEPs in one cell
 all_MEPs = { MEPs_high ; MEPs_low};
+
+%save 
 save([pathOut 'ppTMS_MEPs.mat']);
