5a2447ddb1f8a182082c4e45a71b2d6efb0d3260
manabm <mana.biabani@gmail.com>
2017-11-30 11:54:23 +1100

    Another script has been made for pop_tesa_compselect called pop_tesa_compselect_mana which gets the components from tesa_compselect_mana instead of tesa_compselect. To find and exclude the components from control condition (by FASTICA2)  pop_tesa_compselect should be replaced by pop_tesa_compselect_mana.

--- Mana_TMS_EEG_6.m
+++ Mana_TMS_EEG_6.m
@@ -1,12 +1,11 @@
 clear; close all; clc;
 
-%##### STEP 6: REMOVE ALL OTHER ARTIFACTS #####
+%##### STEP 6: Remove all other artifacts #####
 
-% IDs of participants to analyse
-%  ID = {'002';'003';'004';'005';'006';'007';'008';'009';'010';'011';'012';'013';'014';'015';'016';'017';'018';'019';'020';'021';'022';'022';'023';'024';'025';'026';'026';'027';'028';'029';'030'};
-ID = {'002'};
+%IDs of participants to analyse
+ID = {'001';'002';'003';'004';'005';'006';'007';'008';'009';'010';'011';'012';'013';'014';'015';'016';'017';'019';'020';'021'};
 
-pathOut = '/Volumes/BACKUP_HD/MANA_IHI/Analyzed/';
+pathOut = '/Volumes/BACKUP_HD/MANA_TMS_EEG/Analyzed/';
 
 eeglab;
 
@@ -16,13 +15,11 @@ for idx = 1:length(ID)
     EEG = pop_loadset('filepath',[pathOut,ID{idx,1},'/'],'filename', [ID{idx,1} '_all_blocks_ds_reject_ICA1_clean_ICA2.set']);
     
     %Run FastICA (round 2)
-    EEG = pop_tesa_compselect( EEG,'comps',[],'figSize','small','plotTimeX',[-200 500],'plotFreqX',[1 100],'tmsMuscle','on','tmsMuscleThresh',8,'tmsMuscleWin',[11 30],'tmsMuscleFeedback','off','blink','on','blinkThresh',2.5,'blinkElecs',{'Fp1','Fp2'},'blinkFeedback','off','move','on','moveThresh',2,'moveElecs',{'F7','F8'},'moveFeedback','off','muscle','on','muscleThresh',0.6,'muscleFreqWin',[30 100],'muscleFeedback','off','elecNoise','on','elecNoiseThresh',4,'elecNoiseFeedback','off' );
+     EEG = pop_tesa_compselect_mana( EEG,'comps',[],'figSize','small','plotTimeX',[-200 500],'plotFreqX',[1 100],'tmsMuscle','on','tmsMuscleThresh',8,'tmsMuscleWin',[11 30],'tmsMuscleFeedback','off','blink','on','blinkThresh',2.5,'blinkElecs',{'Fp1','Fp2'},'blinkFeedback','off','move','on','moveThresh',2,'moveElecs',{'F7','F8'},'moveFeedback','off','muscle','on','muscleThresh',0.6,'muscleFreqWin',[30 100],'muscleFeedback','off','elecNoise','on','elecNoiseThresh',4,'elecNoiseFeedback','off' );
+    %To exclude components from control condition 
+    %EEG = pop_tesa_compselect_mana( EEG,'comps',[],'figSize','small','plotTimeX',[-200 500],'plotFreqX',[1 100],'tmsMuscle','on','tmsMuscleThresh',8,'tmsMuscleWin',[11 30],'tmsMuscleFeedback','off','blink','on','blinkThresh',2.5,'blinkElecs',{'Fp1','Fp2'},'blinkFeedback','off','move','on','moveThresh',2,'moveElecs',{'F7','F8'},'moveFeedback','off','muscle','on','muscleThresh',0.6,'muscleFreqWin',[30 100],'muscleFeedback','off','elecNoise','on','elecNoiseThresh',4,'elecNoiseFeedback','off' );
     
     %Save point
     EEG = pop_saveset( EEG, 'filename', [ID{idx,1} '_all_blocks_ds_reject_ICA1_clean_ICA2_clean'], 'filepath', [pathOut ID{idx,1}]);
     
 end
-
-
-
-
