6daa280634083ff801e071ed1d42cd8eb3416c35
manabm <mana.biabani@gmail.com>
2017-11-30 17:24:53 +1100

    laplacian added to this script

--- Mana_TMS_EEG_7.m
+++ Mana_TMS_EEG_7.m
@@ -1,6 +1,6 @@
 clear; close all; clc;
 
-%##### STEP 7: Filter, Interpolate Channels, Split Intensities, Average Reference, Reference to Mastoids #####
+%##### STEP 7: Filter, Interpolate Channels, Split Intensities, Average Reference, Reference to Mastoids, Laplacian (takes longer time, might need to consider not running it with the re-referencing methods)#####
 
 % IDs of participants to analyse
 ID = {'001';'002';'003';'004';'005';'006';'007';'008';'009';'010';'011';'012';'013';'014';'015';'016';'017';'019';'020';'021'};
@@ -28,20 +28,24 @@ for idx = 1:length(ID)
     %Interpolate missing channels
     EEG = pop_interp(EEG, EEG.allchan, 'spherical');
     
-        for cond =  1:length(condition)
-
-            %Extract the data from each condition
-            EEG1 = pop_selectevent( EEG, 'type',condition{cond},'deleteevents','on','deleteepochs','on','invertepochs','off');
-            EEG1 = pop_saveset( EEG1, 'filename', [ID{idx,1} '_FINAL_', condition{cond}],'filepath', [pathOut ID{idx,1}]);
-
-            %Reference each condition's data to common average
-            EEG1av = pop_reref(EEG1, []);
-            EEG1av = pop_saveset(EEG1av, 'filename', [ID{idx,1} '_FINAL_', condition{cond},'_avref'],'filepath', [pathOut ID{idx,1}]);
-
-            %Reference each condition's data to average mastoids
-            EEG1mast = pop_reref( EEG1, [29,30]);
-            EEG1mast = pop_saveset( EEG1mast, 'filename', [ID{idx,1} '_FINAL_', condition{cond}, '_mastref'],'filepath', [pathOut ID{idx,1}]);
-
-        end
+    for cond =  1:length(condition)
+        
+        %Extract the data from each condition
+        EEG1 = pop_selectevent( EEG, 'type',condition{cond},'deleteevents','on','deleteepochs','on','invertepochs','off');
+        
+        % Reference each condition's data to common average and save
+        EEG1av = pop_reref(EEG1, []);
+        EEG1av = pop_saveset(EEG1av, 'filename', [ID{idx,1} '_FINAL_', condition{cond},'_avref'],'filepath', [pathOut ID{idx,1}]);
+        
+        % Reference each condition's data to average mastoids and save
+        EEG1mast = pop_reref( EEG1, [29,30]);
+        EEG1mast = pop_saveset( EEG1mast, 'filename', [ID{idx,1} '_FINAL_', condition{cond}, '_mastref'],'filepath', [pathOut ID{idx,1}]);
+        
+        % Compute and save laplacian of TEPs
+        EEG1lap.data = double(mean(EEG1.data,3));
+        EEG1lap.data = del2map(EEG1.data,EEG1.chanlocs);
+        EEG1lap = pop_saveset(EEG1av, 'filename', [ID{idx,1} '_FINAL_', condition{cond},'_laplac'],'filepath', [pathOut ID{idx,1}]);
+        
+    end
     
 end
