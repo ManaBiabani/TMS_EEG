9d346d71eeab9edd3390bd87fa933211e64f7381
manabm <mana.biabani@gmail.com>
2017-12-08 15:00:23 +1100

    cleaned

--- Mana_Corr_TEPs_MEPs_10.m
+++ Mana_Corr_TEPs_MEPs_10.m
@@ -7,54 +7,41 @@ clear all; close all; clc;
 load('/Volumes/BACKUP_HD/MANA_TMS_EEG/Analyzed/ppTMS_MEPs.mat');
 pathOut = '/Volumes/BACKUP_HD/MANA_TMS_EEG/Analyzed/';
 
-%RefName = 'Laplac.set'
-%RefName = 'Mastref.set'
-RefName = 'avref.set'
+%RefName = 'Laplac';
+%RefName = 'Mastref';
+RefName = 'avref';
 
 % Number of channels
-if strcmp(RefName, 'mastref.set')
-    nbchan = 60
+if strcmp(RefName, 'mastref')
+    nbchan = 60;
 else
-    nbchan = 62
+    nbchan = 62;
 end
 
 %Load TEPs
-if strcmp(RefName, 'mastref.set')
-    load([pathOut 'mastref_TEPs.mat'])
-elseif strcmp(RefName, 'laplac.set')
-    load([pathOut 'laplac_TEPs.mat'])
-elseif strcmp(RefName, 'avref.set')
-    load ([pathOut 'avref_TEPs.mat'])
-end
+load([pathOut, RefName '_TEPs.mat'])
 
 % define conditions
 cond = {'high';'low'};
 
 % for each channel calculate both spearman and pearson correlations between MEPs and TEPs at each ISI for the  different conditions
-
 for int = 1:length(cond)
-    condition = cell2mat(all_timepoints (int));
+    Condition = cell2mat(all_timepoints (int));
+    ControlCondition = cell2mat(all_timepoints (3));
     MEP = cell2mat(all_MEPs(int));
-       
+    
     for j = 1:nbchan
         
         for  isi = 1 :length(ISIs)
-            [CorrSpearman{int}(j,isi), PvalSpearman{int}(j,isi)] = corr(condition(j,:,isi)',(MEP(:,isi)),'type','Spearman');
-            [CorrPearson{int}(j,isi), PvalPearson{int}(j,isi)] = corr(condition(j,:,isi)',(MEP(:,isi)));
+            [CorrSpearman{int}(j,isi), PvalSpearman{int}(j,isi)] = corr(Condition(j,:,isi)',(MEP(:,isi)),'type','Spearman');
+            [CorrPearson{int}(j,isi), PvalPearson{int}(j,isi)] = corr(Condition(j,:,isi)',(MEP(:,isi)));
             %for control
-            [CorrSpearmanControl{int}(j,isi), PvalSpearmanControl{int}(j,isi)] = corr(condition(j,:,isi)',(MEP(:,isi)),'type','Spearman');
-            [CorrSpearmanControl{int}(j,isi), PvalPearsonControl{int}(j,isi)] = corr(condition(j,:,isi)',(MEP(:,isi)));
+            [CorrSpearmanControl{int}(j,isi), PvalSpearmanControl{int}(j,isi)] = corr(ControlCondition(j,:,isi)',(MEP(:,isi)),'type','Spearman');
+            [CorrPearsonControl{int}(j,isi), PvalPearsonControl{int}(j,isi)] = corr(ControlCondition(j,:,isi)',(MEP(:,isi)));
         end
         
     end
 end
 
 %Save
-if strcmp(RefName, 'mastref.set')
-    save ([pathOut, 'mastref_Correlations_MEPs_TEPs']);   
-elseif strcmp(RefName, 'laplac.set')
-    save([pathOut 'laplac_Correlations_MEPs_TEPs']);
-elseif strcmp(RefName, 'avref.set')
-    save ([pathOut, 'avref_Correlations_MEPs_TEPs']);
-end
-
+save ([pathOut, RefName '_Correlations_MEPs_TEPs']);
